2b519029e5aa71df933bb7b24a634901b8617ac4
jordan-m-finn <jordan.m.finn17@gmail.com>
2025-12-21 22:17:56 -0500

    Implement token purchase functionality

--- packages/hardhat/contracts/Vendor.sol
+++ packages/hardhat/contracts/Vendor.sol
@@ -5,15 +5,30 @@ pragma solidity 0.8.20; //Do not change the solidity version as it negatively im
 import "./YourToken.sol";
 
 contract Vendor {
-    // event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
+    event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
 
     YourToken public yourToken;
 
     constructor(address tokenAddress) {
         yourToken = YourToken(tokenAddress);
-    }
+    } 
 
+    uint256 public constant tokensPerEth = 100;
     // ToDo: create a payable buyTokens() function:
+    function buyTokens() public payable {
+        // To find the amount of tokens to dispersed by the amount of ETH the user has deposited:
+        //   (100 / .01) = 10, therefore, 10 tokens is amountOfTokens 
+        uint256 amountOfTokens = (tokensPerEth / msg.value);
+        // To find the amount of ETH the user has deposited (msg.value)
+        uint256 amountOfEth = msg.value;
+         
+        // 1. purchase tokens (10 tokens = 0.1 ETH, 100 tokens = 1 ETH)
+        // yourToken(address, uint256) 
+        yourToken.transfer(msg.sender, amountOfTokens);
+
+        // 2. emit event
+        emit BuyTokens(msg.sender, amountOfEth, amountOfTokens);
+    }
 
     // ToDo: create a withdraw() function that lets the owner withdraw ETH
 
