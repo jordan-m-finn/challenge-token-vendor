9f53924610875309b3194206bf5de772d14475d7
jordan-m-finn <jordan.m.finn17@gmail.com>
2025-12-23 20:25:15 -0500

    Disable deployer sending tokens

--- packages/hardhat/deploy/01_deploy_vendor.ts
+++ packages/hardhat/deploy/01_deploy_vendor.ts
@@ -37,9 +37,9 @@ const deployVendor: DeployFunction = async function (hre: HardhatRuntimeEnvironm
   const vendor = await hre.ethers.getContract<Contract>("Vendor", deployer);
   const vendorAddress = await vendor.getAddress();
   // Transfer tokens to Vendor
-  await yourToken.transfer(vendorAddress, hre.ethers.parseEther("1000"));
+  // await yourToken.transfer(vendorAddress, hre.ethers.parseEther("1000"));
   // Transfer contract ownership to your frontend address
-  await vendor.transferOwnership("0x2797593Ea5D938fAaAF4af33E8E30F52D18b9972");
+  // await vendor.transferOwnership("0x2797593Ea5D938fAaAF4af33E8E30F52D18b9972");
 };
 
 export default deployVendor;
