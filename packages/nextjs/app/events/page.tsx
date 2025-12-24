b5e2a89f799f5a026e4f2cd129b1306512fb8f15
jordan-m-finn <jordan.m.finn17@gmail.com>
2025-12-23 21:47:18 -0500

    Expose sell tokens event component to the UI

--- packages/nextjs/app/events/page.tsx
+++ packages/nextjs/app/events/page.tsx
@@ -66,7 +66,7 @@ const Events: NextPage = () => {
       </div>
 
       {/* SellTokens Events */}
-      {/* <div className="mt-14">
+       <div className="mt-14">
         <div className="text-center mb-4">
           <span className="block text-2xl font-bold">Sell Token Events</span>
         </div>
@@ -108,7 +108,7 @@ const Events: NextPage = () => {
             </table>
           </div>
         )}
-      </div> */}
+      </div>
     </div>
   );
 };
